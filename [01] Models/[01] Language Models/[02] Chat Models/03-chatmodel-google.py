4ff246b56009c7301a0273661d01cd2c272a2ea1
Ahbaz <ahbazmemon0@gmail.com>
2025-11-23 23:35:28 +0530

    demo of chat model llm, google, closed source

--- [01] Models/[01] Language Models/[02] Chat Models/03-chatmodel-google.py
+++ [01] Models/[01] Language Models/[02] Chat Models/03-chatmodel-google.py
@@ -1,2 +1,11 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
+
+load_dotenv()
+
+chat_model = ChatGoogleGenerativeAI("gemini-1.5-pro")
+
+result = chat_model.invoke("What is the capital of India")
+
+# result is dict which have metadata too
+print(result.content)
