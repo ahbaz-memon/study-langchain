ba702d58ecde230bb356a00f23cd62e8ca8701f7
Ahbaz <ahbazmemon0@gmail.com>
2025-11-24 20:36:45 +0530

    setup for endpoint and chatmodel

---
+++ [01] Models/[01] Language Models/[02] Chat Models/04-chatmodel-huggingface-api.py
@@ -0,0 +1,11 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# a end-point from HuggingFace to us
+llm = HuggingFaceEndpoint(
+    repo_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+    task="text-generation",
+)
+chat_model = ChatHuggingFace(llm=llm)
