33b8fde83fc664a72c9707195b3a248c2f7a95a5
Ahbaz <ahbazmemon0@gmail.com>
2025-11-27 14:58:50 +0530

    demo of embedding for docs, huggingface, open source

--- [01] Models/[02] Embedding Models/03-embedding-huggingface-local.py
+++ [01] Models/[02] Embedding Models/03-embedding-huggingface-local.py
@@ -5,7 +5,15 @@ os.environ['HF_HOME'] = "D:/Future Code Study Folder/Libraries/LangChain/Models/
 
 embedding_model = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
 
-text = "Delhi is the capital of India"
+# text = "Delhi is the capital of India"
 
-result = embedding_model.embed_query(text)
+# result = embedding_model.embed_query(text)
+# print(result)
+
+documents = [
+    "Python is a popular framework",
+    "Lion is a animal",
+    "Tiger is a animal"
+]
+result = embedding_model.embed_documents(documents)
 print(result)
