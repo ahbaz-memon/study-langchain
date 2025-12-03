7a608faad8123d35bd3af55373de2c33b5a51eb2
Ahbaz <ahbazmemon0@gmail.com>
2025-12-03 21:07:27 +0530

    creating prompt template

--- [02] Prompts/[01] Single Turn/02-dynamic-prompt.py
+++ [02] Prompts/[01] Single Turn/02-dynamic-prompt.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import PromptTemplate
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -26,3 +27,19 @@ length_input = st.selectbox("Select Explanation Length", options=[
     "Medium (3-5 paragraphs)",
     "Long (detailed explanation)",
 ])
+
+prompt_template = PromptTemplate(
+    template="""
+        Please summarize the research paper titled "{paper_input}" with the following specifications:
+        Explanation Style: {style_input}  
+        Explanation Length: {length_input}  
+        1. Mathematical Details:  
+        - Include relevant mathematical equations if present in the paper.  
+        - Explain the mathematical concepts using simple, intuitive code snippets where applicable.  
+        2. Analogies:  
+        - Use relatable analogies to simplify complex ideas.  
+        If certain information is not available in the paper, respond with: "Insufficient information available" instead of guessing.  
+        Ensure the summary is clear, accurate, and aligned with the provided style and length.
+    """,
+    input_variables=["paper_input", "style_input", "length_input"]
+)
