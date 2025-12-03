e0256ab678591390441944b5cdef7cdb5a7a416a
Ahbaz <ahbazmemon0@gmail.com>
2025-12-03 23:43:29 +0530

    chatbot with langchain messages

--- [02] Prompts/[02] Multi Turn/03-chatbot-with-messages.py
+++ [02] Prompts/[02] Multi Turn/03-chatbot-with-messages.py
@@ -8,12 +8,21 @@ chat_model = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
 
 # history with langchain nomenclature
 chat_messages = [
-    SystemMessage(content="You are a helpful Tutor"),
-    HumanMessage(content="Tell me what is 3D plane")
+    SystemMessage(content="You are a helpful Doctor"),
 ]
 
-result = chat_model.invoke(chat_messages)
-ai_msg = AIMessage(content=result.content)
-chat_messages.append(ai_msg)
+# chatbot with langchain messages
+while True:
+    user_input = input("You: ").strip()
+    user_msg = HumanMessage(content=user_input)
+    chat_messages.append(user_msg)
+
+    if user_input in ["exit", 'e', "quit", "q"]:
+        break
+
+    result = chat_model.invoke(chat_messages)
+    ai_msg = AIMessage(content=result.content)
+    chat_messages.append(ai_msg)
+    print("AI: ", result.content)
 
 print(chat_messages)
