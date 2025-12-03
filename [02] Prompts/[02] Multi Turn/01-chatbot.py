3ff6e97a36b0dbd3dc95dfdd9a84955834f80309
Ahbaz <ahbazmemon0@gmail.com>
2025-12-03 22:18:04 +0530

    a simple chatbot

--- [02] Prompts/[02] Multi Turn/01-chatbot.py
+++ [02] Prompts/[02] Multi Turn/01-chatbot.py
@@ -4,3 +4,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 chat_model = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
+
+# a simple chatbot
+while True:
+    user_input = input('You: ').strip()
+    
+    if user_input in ["exit", 'e', "quit", "q"]:
+        break
+    
+    result = chat_model.invoke(user_input)
+    print("AI: ", result.content)
