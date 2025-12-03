78b88aeddb9857932d2bb7a3022308bef5e5a071
Ahbaz <ahbazmemon0@gmail.com>
2025-12-03 22:59:34 +0530

    chatbot with manual history

--- [02] Prompts/[02] Multi Turn/02-chatbot-with-history.py
+++ [02] Prompts/[02] Multi Turn/02-chatbot-with-history.py
@@ -4,3 +4,18 @@ from dotenv import load_dotenv
 load_dotenv()
 
 chat_model = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
+
+# a simple chatbot with history
+chat_history = []
+while True:
+    user_input = input('You: ').strip()
+    chat_history.append(user_input)
+
+    if user_input in ["exit", 'e', "quit", "q"]:
+        break
+    
+    result = chat_model.invoke(chat_history)
+    chat_history.append(result.content)
+    print("AI: ", result.content)
+
+print(chat_history)
