b74df0719c7093a9b28d8af0502c638883f0c37d
Ahbaz <ahbazmemon0@gmail.com>
2025-12-04 18:55:51 +0530

    current day conversation

--- [02] Prompts/[02] Multi Turn/05-chatbot-with-message-placeholder.py
+++ [02] Prompts/[02] Multi Turn/05-chatbot-with-message-placeholder.py
@@ -1,2 +1,9 @@
-from dotenv import load_dotenv
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+
+# current day conversation
+chat_message_prompt_template = ChatPromptTemplate([
+    ('system', "You are a helpful cutomer support agent"),
+    MessagesPlaceholder(variable_name='old_chat_messages')
+    ('human', '{query}'),
+])
