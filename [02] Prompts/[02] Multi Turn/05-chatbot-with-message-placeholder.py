e943abad8f4b5146b3bfe07d358dcfa6035b7aea
Ahbaz <ahbazmemon0@gmail.com>
2025-12-04 23:29:26 +0530

    demo of message placeholder

--- [02] Prompts/[02] Multi Turn/05-chatbot-with-message-placeholder.py
+++ [02] Prompts/[02] Multi Turn/05-chatbot-with-message-placeholder.py
@@ -4,6 +4,16 @@ from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 # current day conversation
 chat_message_prompt_template = ChatPromptTemplate([
     ('system', "You are a helpful cutomer support agent"),
-    MessagesPlaceholder(variable_name='old_chat_messages')
+    MessagesPlaceholder(variable_name='old_chat_messages'),
     ('human', '{query}'),
 ])
+
+# load old conversation
+with open('../../Data/customer-chat-history.txt') as f:
+    old_chat_messages = f.readlines()
+
+chat_message_prompt = chat_message_prompt_template.invoke({
+    'old_chat_messages': old_chat_messages,
+    'query': "where is my refund ?",
+})
+print(chat_message_prompt)
