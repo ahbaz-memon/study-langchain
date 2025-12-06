8786e719dcdf65432828c634aac802cb42f171c9
Ahbaz <ahbazmemon0@gmail.com>
2025-12-06 23:19:20 +0530

    demo of typed dict

--- [03] Structured Output/[01] Typed Dict/01-typed-dict-demo.py
+++ [03] Structured Output/[01] Typed Dict/01-typed-dict-demo.py
@@ -4,3 +4,12 @@ class Student(TypedDict):
     name: str
     roll_no: int
     age: int
+
+student: Student = {
+    'name': 'Zaid',
+    'roll_no': 72,
+    'age': 19, # str will also work, since TypedDict is guider only
+}
+
+print(student)
+print(type(student))
