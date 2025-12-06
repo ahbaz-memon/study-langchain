bf1c2a6d0070df693ec65dc19ea58fbaf4b8e626
Ahbaz <ahbazmemon0@gmail.com>
2025-12-06 23:47:48 +0530

    created stucture of review

--- [03] Structured Output/[01] Typed Dict/02-simple-typed-dict.py
+++ [03] Structured Output/[01] Typed Dict/02-simple-typed-dict.py
@@ -1,8 +1,18 @@
 from typing import TypedDict
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # loading review
 with open('../../Data/review-1.txt') as f:
     lines = f.readlines()
     review = ' '.join(lines)
 
-print(review)
+# review structure
+class Review(TypedDict):
+    product_name: str
+    summary: str
+    sentiment: bool
+
+chat_model = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
